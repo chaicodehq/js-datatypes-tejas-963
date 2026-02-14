20c75e2944cdb8815101a817830cb4525dd61f70
Tejas-963 <tcodercpp233@gmail.com>
2026-02-14 10:30:27 +0530

    completed gst calculator problem

--- src/04-gst-calculator.js
+++ src/04-gst-calculator.js
@@ -39,5 +39,45 @@
  *   // => { baseAmount: 500, gstRate: 0, gstAmount: 0, totalAmount: 500 }
  */
 export function calculateGST(amount, category) {
-  // Your code here
+  
+  if(!amount || !isFinite(amount) || amount < 0 || typeof category !== "string") return null
+
+  let gstRate;
+
+  switch (category.toLowerCase()) {
+		case "essential":
+			gstRate = 0;
+			break;
+
+		case "food":
+			gstRate = 5;
+			break;
+
+		case "standard":
+			gstRate = 12;
+			break;
+
+		case "electronics":
+			gstRate = 18;
+			break;
+
+		case "luxury":
+			gstRate = 28;
+			break;
+
+		default:
+      return null
+  }
+
+  const gstAmount = parseFloat((amount * gstRate / 100).toFixed(2))
+
+  const totalAmount = parseFloat((amount + gstAmount).toFixed(2))
+
+  return {
+		baseAmount: amount,
+		gstRate,
+		gstAmount,
+		totalAmount,
+  }
+
 }
